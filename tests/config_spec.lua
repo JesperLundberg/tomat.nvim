c4fed72b652af8768daccd663fc6a9191720fdcf
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-05-18 21:19:08 +0200

    fix tests halting for config file

--- tests/config_spec.lua
+++ tests/config_spec.lua
@@ -19,84 +19,84 @@ describe("config", function()
 	end)
 
 	describe("setup", function()
-		-- it("should override default values with supplied values", function()
-		-- 	notifyStub.returns({
-		-- 		notify = {},
-		-- 	})
-		--
-		-- 	-- Override all default values
-		-- 	sut.setup({
-		-- 		session_time_in_minutes = 60,
-		-- 		automatic = {
-		-- 			create_a_new_session = true,
-		-- 			break_time_in_minutes = 10,
-		-- 		},
-		-- 		icon = {
-		-- 			in_progress = "",
-		-- 			done = "",
-		-- 		},
-		-- 	})
-		--
-		-- 	-- Check if the default values are overridden
-		-- 	assert.are.same({
-		-- 		session_time_in_minutes = 60,
-		-- 		automatic = {
-		-- 			create_a_new_session = true,
-		-- 			break_time_in_minutes = 10,
-		-- 		},
-		-- 		icon = {
-		-- 			in_progress = "",
-		-- 			done = "",
-		-- 		},
-		-- 	}, sut.options)
-		-- end)
+		it("should override default values with supplied values", function()
+			notifyStub.returns({
+				notify = {},
+			})
 
-		-- 	it("should not override default values with nil", function()
-		-- 		-- Override all default values with nil
-		-- 		sut.setup({
-		-- 			session_time_in_minutes = nil,
-		-- 			automatic = {
-		-- 				create_a_new_session = nil,
-		-- 				break_time_in_minutes = nil,
-		-- 			},
-		-- 			icon = {
-		-- 				in_progress = nil,
-		-- 				done = nil,
-		-- 			},
-		-- 		})
-		--
-		-- 		-- Check if the default values are not overridden
-		-- 		assert.are.same({
-		-- 			session_time_in_minutes = 30,
-		-- 			automatic = {
-		-- 				create_a_new_session = false,
-		-- 				break_time_in_minutes = 5,
-		-- 			},
-		-- 			icon = {
-		-- 				in_progress = "",
-		-- 				done = "",
-		-- 			},
-		-- 		}, sut.options)
-		-- 	end)
-		--
-		-- 	it("should override only the supplied values", function()
-		-- 		-- Override only the supplied values
-		-- 		sut.setup({
-		-- 			session_time_in_minutes = 60,
-		-- 		})
-		--
-		-- 		-- Check if the default values are overridden
-		-- 		assert.are.same({
-		-- 			session_time_in_minutes = 60,
-		-- 			automatic = {
-		-- 				create_a_new_session = false,
-		-- 				break_time_in_minutes = 5,
-		-- 			},
-		-- 			icon = {
-		-- 				in_progress = "",
-		-- 				done = "",
-		-- 			},
-		-- 		}, sut.options)
-		-- 	end)
+			-- Override all default values
+			sut.setup({
+				session_time_in_minutes = 60,
+				automatic = {
+					create_a_new_session = true,
+					break_time_in_minutes = 10,
+				},
+				icon = {
+					in_progress = "",
+					done = "",
+				},
+			})
+
+			-- Check if the default values are overridden
+			assert.are.same({
+				session_time_in_minutes = 60,
+				automatic = {
+					create_a_new_session = true,
+					break_time_in_minutes = 10,
+				},
+				icon = {
+					in_progress = "",
+					done = "",
+				},
+			}, sut.options)
+		end)
+
+		it("should not override default values with nil", function()
+			-- Override all default values with nil
+			sut.setup({
+				session_time_in_minutes = nil,
+				automatic = {
+					create_a_new_session = nil,
+					break_time_in_minutes = nil,
+				},
+				icon = {
+					in_progress = nil,
+					done = nil,
+				},
+			})
+
+			-- Check if the default values are not overridden
+			assert.are.same({
+				session_time_in_minutes = 30,
+				automatic = {
+					create_a_new_session = false,
+					break_time_in_minutes = 5,
+				},
+				icon = {
+					in_progress = "",
+					done = "",
+				},
+			}, sut.options)
+		end)
+
+		it("should override only the supplied values", function()
+			-- Override only the supplied values
+			sut.setup({
+				session_time_in_minutes = 60,
+			})
+
+			-- Check if the default values are overridden
+			assert.are.same({
+				session_time_in_minutes = 60,
+				automatic = {
+					create_a_new_session = false,
+					break_time_in_minutes = 5,
+				},
+				icon = {
+					in_progress = "",
+					done = "",
+				},
+			}, sut.options)
+		end)
 	end)
 end)
--- tests/minimal.vim
+++ tests/minimal.vim
@@ -1,4 +1,4 @@
 set rtp+=.
 set rtp+=~/.local/share/nvim/lazy/plenary.nvim
-" set rtp+=~/.local/share/nvim/lazy/mini.pick
+set rtp+=~/.local/share/nvim/lazy/nvim-notify
 runtime! plugin/plenary.vim
